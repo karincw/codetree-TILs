d0ea7041212f8a5e09c90ff71117492e2fbbbe49
Karin <99000645+karincw@users.noreply.github.com>
2024-08-09 15:41:59 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240809/4번의 크기 비교/4-time-comparison.cpp
+++ 240809/4번의 크기 비교/4-time-comparison.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main() {
+
     int a,b,c,d,e;
     std::cin >> a >> b >> c >> d >> e;
-    std:cout << (a > b ? : 1 : 0) << "\n";
-    std:cout << (a > c ? : 1 : 0) << "\n";
-    std:cout << (a > d ? : 1 : 0) << "\n";
-    std:cout << (a > e ? : 1 : 0) << "\n";
+
+    std:cout << (a > b ?  1 : 0) << "\n";
+    std:cout << (a > c ?  1 : 0) << "\n";
+    std:cout << (a > d ?  1 : 0) << "\n";
+    std:cout << (a > e ?  1 : 0) << "\n";
+
     return 0;
 }
