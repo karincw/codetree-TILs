13ca947d31abad0a45838b1225cde27016cfadf9
Karin <99000645+karincw@users.noreply.github.com>
2024-08-09 15:36:09 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/두 번의 연산/two-operations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    std::cin >>a;
+
+    if(a | 1 == 1)
+    {
+        a *= 3;
+    }
+    if(a % 3 == 0)
+    {
+        a/=3;
+    }
+
+    std::cout << a;
+    
+
+    return 0;
+
+}
