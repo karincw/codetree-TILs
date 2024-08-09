ebd4e2eadeff314d8793188c5f5a5a7e8f838816
Karin <99000645+karincw@users.noreply.github.com>
2024-08-09 16:06:14 +0900

    Title: 둘 중 하나의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240809/둘 중 하나의 배수/multiple-of-either.cpp
+++ 240809/둘 중 하나의 배수/multiple-of-either.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a;
     std::cin >>a;
-    std::cout << ( a % 3 == 0 && a % 5 == 0? 1:0);
+    std::cout << ( a % 3 == 0 || a % 5 == 0? 1:0);
     return 0;
 }
