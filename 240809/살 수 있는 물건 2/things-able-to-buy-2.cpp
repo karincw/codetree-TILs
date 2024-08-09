46e67c99c38226a195ebb32acdbffb2ba4e9a6a4
Karin <99000645+karincw@users.noreply.github.com>
2024-08-09 15:33:54 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n ;
+    std::cin >> n;
+    if(n >= 3000)
+    std::cout << "book";
+    else if (n >= 1000)
+    std::cout << "mask";
+    else if(n >= 500)
+    std::cout << "pen";
+    else
+    std::cout << "no";
+    return 0;
+}
