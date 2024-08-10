661f676b18a2128fb1b8eccc0840841c24bd05a6
Karin <99000645+karincw@users.noreply.github.com>
2024-08-10 12:10:21 +0900

    Title: a의 배수들의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240810/a의 배수들의 곱/product-of-multiples-of-a.cpp
+++ 240810/a의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     int a,b,r = 1;
     std::cin >> a>>b;
-    for(int i = 1; i < b; ++i)
+    for(int i = 1; i <= b; ++i)
     {
         if(i % a == 0)
         {
             r *= i;
         }
     }
-    std::cout<<r;
+    std::cout << r;
     return 0;
 }
