259ac4db15d113798b263c0f7fda9a9316cf7fed
Karin <99000645+karincw@users.noreply.github.com>
2024-08-10 12:00:04 +0900

    Title: 등장횟수 확인, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240810/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e,r=0;
+    std::cin >>a>>b>>c>>d>>e;
+    if(a % 2 == 0)
+        r++
+    if(b % 2 == 0)
+        r++
+    if(c % 2 == 0)
+        r++
+    if(d % 2 == 0)
+        r++
+    if(e % 2 == 0)
+        r++
+
+        std::cout << r;
+    return 0;
+}
