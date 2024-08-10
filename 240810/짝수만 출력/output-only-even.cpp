2f98bc6ae91180e3a428fbdd89651e83318e1698
Karin <99000645+karincw@users.noreply.github.com>
2024-08-10 11:39:09 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240810/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    std::cin >> a >> b;
+    for (int i = a; i <= b; i++)
+        if(i % 2 == 0)
+            std::cout << i << " ";
+    return 0;
+}
