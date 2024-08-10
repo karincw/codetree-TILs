34bf026edc0232bfe1bb35dedb67b26ba094981e
Karin <99000645+karincw@users.noreply.github.com>
2024-08-10 12:04:07 +0900

    Title: 총합 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240810/총합 구하기/find-the-total.cpp
+++ 240810/총합 구하기/find-the-total.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    int a,b;
+    int a,b,r = 0;
     std::cin >> a >> b;
     for (int i = a; i <= b; i++)
     {
         if(i % 6 ==0 && i % 8 != 0)
         {
-            std::cout << i;
-            break;
+            r += i;
         }
     }
+    std::Cout << r;
     return 0;
 }
