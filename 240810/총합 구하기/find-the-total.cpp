1abfce7f7aa682abb8b1f76e3793bda975b68f52
Karin <99000645+karincw@users.noreply.github.com>
2024-08-10 12:03:32 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/총합 구하기/find-the-total.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    std::cin >> a >> b;
+    for (int i = a; i <= b; i++)
+    {
+        if(i % 6 ==0 && i % 8 != 0)
+        {
+            std::cout << i;
+            break;
+        }
+    }
+    return 0;
+}
