0d0ba9628d8aeb919bf3782e748a3de1c0a623e4
Karin <99000645+karincw@users.noreply.github.com>
2024-08-10 12:00:21 +0900

    Title: 등장횟수 확인, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240810/등장횟수 확인/check-number-of-appearances.cpp
+++ 240810/등장횟수 확인/check-number-of-appearances.cpp
@@ -5,15 +5,15 @@ int main() {
     int a,b,c,d,e,r=0;
     std::cin >>a>>b>>c>>d>>e;
     if(a % 2 == 0)
-        r++
+        r++;
     if(b % 2 == 0)
-        r++
+        r++;
     if(c % 2 == 0)
-        r++
+        r++;
     if(d % 2 == 0)
-        r++
+        r++;
     if(e % 2 == 0)
-        r++
+        r++;
 
         std::cout << r;
     return 0;
