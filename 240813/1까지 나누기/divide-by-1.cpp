3c19a4a71beccac3f90f97f8ac4a524c7081193e
Karin <99000645+karincw@users.noreply.github.com>
2024-08-13 09:54:18 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240813/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n;
+    std::cin >> n;
+
+    int result = 0;
+    for (int i = 1; n >= 1; i++)
+    {
+        if(n >= 1)
+        {
+            result++;
+        }
+        n /= i;
+    }
+    std::cout << result;
+    
+    return 0;
+}
